6d54bffd24bf253bd8d50866213474f7b4956dbd
VitaminK2001 <1820011129@qq.com>
2022-12-15 09:16:41 +0800

    update algoNotes

--- AlgoNotes/Leetcode/String/1945字符串转换后各位数字之和/Solution.c
+++ AlgoNotes/Leetcode/String/1945字符串转换后各位数字之和/Solution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int getLucky(char * s, int k){
     int n = strlen(s);
     char digits[n*2+1];
@@ -16,4 +17,13 @@ int getLucky(char * s, int k){
         len = sprintf(digits, "%d", sum); //如果陷入的字符数量只有一位则不能进行运算求和了
     }
     return atoi(digits);
+}
+
+int main(){
+    char *s = (char*) malloc(sizeof(char)*20);
+    int res = sprintf(s, "%s", "we are champion");
+    printf("%d", res); //15
+    printf("%s\n", s); //we are the champion
+    //将数字字符串转换为数字 库函数stdlib.h
+    printf("%d", atoi("985")); //985
 }
