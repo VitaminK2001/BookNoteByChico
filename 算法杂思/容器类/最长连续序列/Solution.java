657a2545cbad131425cdd17a1ef877f20a5f7492
VitaminK2001 <1820011129@qq.com>
2023-01-24 13:06:06 +0800

    update 容器类

---
+++ 算法杂思/容器类/最长连续序列/Solution.java
@@ -0,0 +1,26 @@
+package 算法杂思.容器类.最长连续序列;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+        int res = 0;
+        for(int num : nums) {
+            if(!set.contains(num-1)) {
+                int cur = num;
+                int path = 1;
+                while(set.contains(cur+1)) {
+                    cur += 1;
+                    path += 1;
+                }
+                res = Math.max(res, path);
+            }
+        } 
+        return res;
+    }
+}
